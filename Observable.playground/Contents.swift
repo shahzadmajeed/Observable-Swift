c0de1d570aa9979c50490c7be201f99c4d22cd2d
Shahzad Majeed <smajeed@draftkings.com>
2017-07-06 19:19:24 -0400

    some updates

--- Observable.playground/Contents.swift
+++ Observable.playground/Contents.swift
@@ -2,26 +2,31 @@
 
 import Observable
 
-struct Person {
-    var cgpa: Double
+class Person {
+    var cgpa: Observable<Double>
     var first: Observable<String>
     var last: Observable<String>
-    
-    init(first: String, last: String, cgpa: Double) {
-        self.cgpa = cgpa
+    var college: String
+    init(first: String, last: String, cgpa: Double, college: String) {
+        self.cgpa = Observable(cgpa)
+        self.college = college
         self.first = Observable(first)
         self.last = Observable(last)
     }
 }
 
-var p = Person(first: "Shahzad", last: "Majeed", cgpa: 3.1)
+var p = Person(first: "Shahzad", last: "Majeed", cgpa: 3.1, college: "FAST")
 
 p.first.afterChange += printNewValue
-
 p.last.afterChange += printNewValue
+p.cgpa.afterChange += { oldValue, newValue in
+    print("\(oldValue) updated to \(newValue)")
+    // Update UI here
+}
 
 p.first.value = "Abbass"
 p.last.value = "Majeed"
+p.cgpa.value = 4.0
 
 func printNewValue(oldValue: Any, newValue: Any) {
     print("\(oldValue) updated to \(newValue)")
